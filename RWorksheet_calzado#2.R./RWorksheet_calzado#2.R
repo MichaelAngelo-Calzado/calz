6eebd648c06a8effab2250366a24d335820965fb
MIchael Angelo S. Calzado <michaelangelo.calzado@students.isatu.edu.ph>
2024-09-25 02:15:39 +0000

    Calzado Worksheet2

---
+++ RWorksheet_calzado#2.R./RWorksheet_calzado#2.R
@@ -0,0 +1,15 @@
+Using Vectors
+1. Create a vector using : operator
+a. Sequence from -5 to 5. Write the R code and its output.
+Describe its output.
+
+```{r}
+vec <- -5:5
+vec
+```
+b. x <- 1:7. What will be the value of x?
+  
+```{r}
+
+
+```
